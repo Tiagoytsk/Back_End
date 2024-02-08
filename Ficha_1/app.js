afd0efb0147ade67e3de29ee04d731d345f58686
Tiagoytsk <tiagompedra@gmail.com>
2024-02-08 11:52:20 +0000

    final aula 08/02/2024

--- Ficha_1/app.js
+++ Ficha_1/app.js
@@ -1,7 +1,20 @@
 function notafinal(nt,np){
-    nt=nt/2
-    np=np/2
-    nf=nt+np
-    return nf>=9.5
+    var mean= nt*0.4+np*0.6;
+    console.log("Your average is:"+mean);
+    if (mean<9.5){
+        console.log("you failed")
+    }
+    else{
+        console.log("you passed")
+    }
 }
-console.log(notafinal(10,10));
+notafinal(8,10);
+
+function mes(num){
+    var mes=["janeiro","fevereiro","marco","abril","maio","junho","julho","agosto","setembro","outrubro","novembro","dezembro"]
+    console.log(mes[num-1]);
+}
+mes(8);
+
+
+
