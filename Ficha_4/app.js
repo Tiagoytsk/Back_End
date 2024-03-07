0165ba4406c1b6b2f6de784ece087d79d1f6b3a7
Tiagoytsk <tiagompedra@gmail.com>
2024-03-07 09:37:56 +0000

    ficha 4

---
+++ Ficha_4/app.js
@@ -0,0 +1,22 @@
+module.exports={
+    env:{
+        DEV1:{
+            host: "DEV688",
+            connectionString:"192.168.0.1@root",
+            password:"fdaffda",
+            region:"US"
+        },
+        TEST:{
+            host: "TEST688",
+            connectionString:"192.168.0.1@root",
+            password:"fdaffda",
+            region:"US"
+        },
+        QA:{
+            host: "QA688",
+            connectionString:"192.168.0.1@root",
+            password:"fdaffda",
+            region:"US"
+        },
+    }
+}
