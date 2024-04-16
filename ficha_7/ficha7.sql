c3217fd42e106c41814d505b6970ee044ed5cbd1
Tiagoytsk <tiagompedra@gmail.com>
2024-04-16 18:36:02 +0100

    gfg

--- ficha_7/ficha7.sql
+++ ficha_7/ficha7.sql
@@ -1,3 +1,5 @@
+ALTER USER 'root'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'Tiagom11$';
+FLUSH PRIVILEGES;
 create schema ficha7;
 
 CREATE TABLE ficha7.People (
@@ -9,7 +11,7 @@ CREATE TABLE ficha7.People (
     PRIMARY KEY (Id)
 );
 
-insert into ficha7.People(Firstname,Lastname,Profession,Age) values(Joao,Paulo,Agricultor,34);
-insert into ficha7.People(Firstname,Lastname,Profession,Age) values(Ana,Malhoua,cantora,44);
-insert into ficha7.People(Firstname,Lastname,Profession,Age) values(Tiago,Pedra,Estudante,24);
-insert into ficha7.People(Firstname,Lastname,Profession,Age) values(Joao,Rodrigo,Advogado,54);
+INSERT INTO ficha7.People(Firstname, Lastname, Profession, Age) VALUES('Joao', 'Paulo', 'Agricultor', 34);
+INSERT INTO ficha7.People(Firstname, Lastname, Profession, Age) VALUES('Ana', 'Malhoua', 'cantora', 44);
+INSERT INTO ficha7.People(Firstname, Lastname, Profession, Age) VALUES('Tiago', 'Pedra', 'Estudante', 24);
+INSERT INTO ficha7.People(Firstname, Lastname, Profession, Age) VALUES('Joao', 'Rodrigo', 'Advogado', 54);
